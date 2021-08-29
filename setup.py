06984fe7ee7a139782c75b40346f13f5c21df20a
symonk <jackofspaces@gmail.com>
2021-08-29 14:27:07 +0100

    adding a development pre-commit hook to automatically setup git hooks for the repo

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
-import setuptools
+import subprocess
 
-# required for installing zentinel as an editable install
-setuptools.setup()
+import setuptools
+from setuptools.command.develop import develop
+
+
+class AutomaticallyInstallPreCommit(develop):
+    """
+    Custom development hook to automatically install pre-commit .githooks into the repository
+    .git/hooks/ directory when doing a development install of zentinel.
+
+        pip install -e ".[tests]"
+            * installs dependencies
+            * installs pre-commit itself
+            * actually installs pre-commit hooks (a subsequent step required)
+    """
+
+    def run(self):
+        super().run()
+        subprocess.run(["pre-commit", "install"])
+
+
+setuptools.setup(cmdclass={"develop": AutomaticallyInstallPreCommit})
