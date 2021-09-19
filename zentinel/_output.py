4dea9066605a112f6bb665a8a94732d685221e9f
symonk <jackofspaces@gmail.com>
2021-09-19 15:16:27 +0100

    starting to build out terminal writing capabilities; and support zentinel invocations programmatically

--- zentinel/_output.py
+++ zentinel/_output.py
@@ -1,2 +1,21 @@
-class TerminalWriter:
+from typing import Protocol
+from typing import runtime_checkable
+
+
+@runtime_checkable
+class Writable(Protocol):
+    def write(self, message: str) -> None:
+        """ Write the message to the chosen stream."""
+
+
+class StdoutStream(Writable):
     ...
+
+
+class FileStream(Writable):
+    ...
+
+
+class TerminalWriter:
+    def __init__(self, writable: Writable) -> None:
+        self.writer = writable
--- zentinel/main.py
+++ zentinel/main.py
@@ -1,13 +1,22 @@
 import asyncio
+import typing
 
 from ._argparse import build_configuration
 from ._scanner import Scanner
 
 
-def main():
+def main(args: typing.Optional[typing.List[str]] = None) -> int:
+    """
+    Main entrypoint into zentinel.  This can be called in two ways.
+    Preferably by running the `zentinel` command line which is registered via the console_scripts
+    entry point, or by calling `zentinel.main()` programmatically within python code.
+
+    :param args: A list of arguments, replacing those which would normally be provided on the command line.
+    :return: An integer representing the exit code of executing zentinel.
+    """
     zentinel_config = build_configuration()
     scanner = Scanner(zentinel_config.target, zentinel_config.ports)
     asyncio.run(scanner.perform_scan())
     print("open port summary".center(100, "-"))
     print(scanner.open_ports)
-    return 125
+    return 0
