ccb6997bf0b49a81df888944a4262828c51603c5
symonk <jackofspaces@gmail.com>
2023-07-22 08:41:25 +0100

    add update to `dependencies.py`

--- scripts/dependencies.py
+++ scripts/dependencies.py
@@ -1,18 +1,30 @@
+#!/bin/env python3
+import argparse
 import subprocess
 import sys
 import typing
 
 
+def build_namespace() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--no-push", "-np", action="store_false", default=True, help="Push the update to remote.", dest="no_push"
+    )
+    return parser.parse_args()
+
+
 def main() -> int:
+    namespace = build_namespace()
     return_code = 0
     return_code += remove_lock_if_exists()
     return_code += poetry_update()
     return_code += pre_commit_update()
     if not return_code:
-        print("Updating dependencies on the remote now.")
-        commit_and_push()
-    else:
-        print("No dependencies to update.")
+        if namespace.no_push:
+            commit_and_push()
+        else:
+            print("Changes detected but script was executed with --no-push so changes remain local.")
+    print(f"Exited: {return_code}")
     return return_code
 
 
@@ -31,21 +43,24 @@ def pre_commit_update():
 def commit_and_push():
     return (
         _run_command(("git", "add", "poetry.lock", ".pre-commit-config.yaml", "pyproject.toml"))
-        + _run_command(("git", "commit", "-m", "`(chore)` :rocket: `dependency upgrades`."))
+        + _run_command(("git", "commit", "-m", ":rocket: `dependency upgrades`."))
         + _run_command(("git", "push"))
     )
 
 
 def _run_command(command: typing.Tuple[str, ...]) -> int:
-    """Run a command and return the subprocess exit code.
-
-    :param command: Command to run.
-    :return:
     """
-    return subprocess.run(command, stdout=sys.stdout, stderr=subprocess.STDOUT).returncode
+    Run a command and return the subprocess exit code.
+    :param command: Command to run.
+    :return: the integer representing the exit code from the subprocess
+    """
+    return subprocess.run(command, check=False, stdout=sys.stdout, stderr=subprocess.STDOUT).returncode
 
 
 if __name__ == "__main__":
-    """A rather naive utility script for updating poetry and pre-commit
-    dependencies."""
+    """
+    A rather naive utility script for updating poetry and pre-commit dependencies.
+    From the root directory of `asserto`:
+        python scripts/dependencies.py
+    """
     raise SystemExit(main())
